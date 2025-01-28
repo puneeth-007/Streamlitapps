071f1cf89af865c8b453527a55a6ce1cb8896c57
puneeth-007 <100269212+puneeth-007@users.noreply.github.com>
2025-01-28 13:35:14 +0530

    stable

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 
 import pandas as pd
-import matplotlib as plt
-import seaborn as sns
 import numpy as np
 import streamlit as st
 
+def dffun(response):
+	df=pd.json_normalize(response.json(),record_path=['data'])
+	return df[['name','full_address']]
+
 st.title('Maps data with address')
-import requests
-url = "https://maps-data.p.rapidapi.com/nearby.php"
+place=st.text_input('Please enter the place you want to search')
+if st.button('search'):
+	import requests
+	url = "https://maps-data.p.rapidapi.com/nearby.php"
+	querystring = {"query":place,"lat":"17.40","lng":"78.47","limit":"100","country":"ind","lang":"en","offset":"0","zoom":"12"}
+	headers = {
+		"x-rapidapi-key": "603806efdcmsh7077e2d3ea167a7p1e2264jsnc4ff6dcdf758",
+		"x-rapidapi-host": "maps-data.p.rapidapi.com"
+		}
+	response = requests.get(url, headers=headers, params=querystring)
 
-querystring = {"query":"bike repair shop","lat":"17.40","lng":"78.47","limit":"100","country":"ind","lang":"en","offset":"0","zoom":"12"}
-
-headers = {
-	"x-rapidapi-key": "603806efdcmsh7077e2d3ea167a7p1e2264jsnc4ff6dcdf758",
-	"x-rapidapi-host": "maps-data.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-#print(response.json())
-
-df=pd.json_normalize(response.json(),record_path=['data'])
-df[['name','full_address']].head
+	#print(response.json())
+	df=pd.json_normalize(response.json(),record_path=['data'])
+	st.write(df[['name','full_address']])

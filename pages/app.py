446bb83db71f271b77375736ab0b930f211648d5
puneeth-007 <100269212+puneeth-007@users.noreply.github.com>
2025-02-07 20:55:59 +0530

    sfd

--- pages/app.py
+++ pages/app.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-import plotly.figure_factory as ff
 import plotly.express as px
+
+st.set_page_config(
+    page_title="Bike Karido",
+    page_icon=":bike:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
 
 st.title('Bike Karido Used bikes data')
 
@@ -56,25 +64,15 @@ if st.button("Fetch data"):
         fdf4 = fdf3
     st.write(fdf4)
 
-  
-    '''fig = px.histogram(fdf, x='Price', title='Histogram of Price')
-    fig.show()
-    st.plotly_chart(fig)'''
-
-    '''st.write(fdf4)
-    fig = ff.create_distplot( fdf4['Price'])
-    st.plotly_chart(fig)'''
     fig = px.scatter(
-    fdf4,
-    x="Price",
-    y="Registration_Year",
-    color="Ownership",size="Price",
-    hover_data=["KMs_Driven","Model","Make"],
+        fdf4,
+        x="Price",
+        y="Registration_Year",
+        color="Ownership",
+        size="Price",
+        hover_data=["KMs_Driven", "Model", "Make"],
     )
     st.plotly_chart(fig, key="Price")
 
-    fig1 = px.sunburst(fdf4,path=['Location','Make','Name'],values='Price',color='Location')
-    st.plotly_chart(fig1,key='Make')
-    
-    '''event = st.plotly_chart(fig, key="Price")
-    event.selection'''
+    fig1 = px.sunburst(fdf4, path=['Location', 'Make', 'Name'], values='Price', color='Location')
+    st.plotly_chart(fig1, key='Make')

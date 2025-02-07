229f69bb7f1d11025fbd96e12862ea3e125ae358
puneeth-007 <100269212+puneeth-007@users.noreply.github.com>
2025-02-07 08:01:59 +0530

    sdf

--- pages/pages/app.py
+++ pages/pages/app.py
@@ -5,29 +5,23 @@ import plotly.express as px
 import webbrowser
 
 st.title('Bike Karido Used bikes data')
-
-# Load the data
 f = pd.read_csv(r'pages/secondhandbike.csv', sep='|')
 fdf = pd.DataFrame(f)
 fdf = fdf.drop(columns='Unnamed: 0')
 
-# Convert columns to numeric
 fdf['Price'] = pd.to_numeric(fdf['Price'], errors='coerce')
 fdf['Registration_Year'] = pd.to_numeric(fdf['Registration_Year'], errors='coerce')
 fdf['KMs_Driven'] = pd.to_numeric(fdf['KMs_Driven'], errors='coerce')
 
-# Default values for price range
 default_price = 10000
 default_price1 = 1000000
 
-# User inputs
 Price_range_lower = st.number_input("Enter the Lower price:", min_value=500, max_value=10000000, key='price_lower', value=default_price)
 Price_range_upper = st.number_input("Enter the Upper price:", min_value=500, max_value=10000000, key='price_upper', value=default_price1)
 ownership = st.selectbox("Select Ownership", ['All','First', 'Second', 'Third', 'Above'])
 company = st.selectbox("Select Company (leave empty to fetch all):", ["All"] + list(fdf['Make'].unique()), key='company')
 registration_year = st.selectbox("Enter Year of Registration:", ["All"] + list(map(str, range(2000, 2025))), key='registration_year')
 
-# Fetch data based on user inputs
 if st.button("Fetch data"):
     fdf_filtered = fdf[(fdf['Price'] >= Price_range_lower) & (fdf['Price'] <= Price_range_upper)]
     if ownership != 'All':
@@ -39,7 +33,7 @@ if st.button("Fetch data"):
     
     st.write(fdf_filtered)
 
-    # Create scatter plot
+    
     fig = px.scatter(
         fdf_filtered,
         x="Price",
@@ -50,10 +44,9 @@ if st.button("Fetch data"):
     )
     st.plotly_chart(fig, key="Price")
 
-    # Create an empty container to display the selected URL
+    
     selected_url_container = st.empty()
 
-    # JavaScript to get selected data point and open URL
     st.markdown("""
     <script type="text/javascript">
         var plotly_chart = document.querySelector("[data-testid='stPlotlyChart']");
